3b36435fbe3059b0e190397db286f625908b77f7
Chirag Sachdeva <chiragsachdeva8150@gmail.com>
2026-04-14 13:26:26 +0530

    fix(admin): resolve blank screen by updating CSP and CORS for railway

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -3,13 +3,30 @@ import type { Core } from '@strapi/strapi';
 const config: Core.Config.Middlewares = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'script-src': ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   {
     name: 'strapi::cors',
     config: {
-      origin: process.env.CORS_ORIGINS
-        ? process.env.CORS_ORIGINS.split(',')
-        : ['http://localhost:3000', 'http://localhost:5173'],
+      origin: [
+        ...(process.env.CORS_ORIGINS ? process.env.CORS_ORIGINS.split(',') : []),
+        process.env.PUBLIC_URL ? process.env.PUBLIC_URL : 'http://localhost:1337',
+        'http://localhost:3000',
+        'http://localhost:5173',
+      ],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
       keepHeaderOnError: true,
